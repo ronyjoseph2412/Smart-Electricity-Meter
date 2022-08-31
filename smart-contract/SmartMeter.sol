c421f95d95490ae733ba240b86935b1df9a411be
Yathin Prakash K <yathinprakash017@gmail.com>
2022-08-31 19:10:17 +0530

    Added Smart Contract

--- smart-contract/SmartMeter.sol
+++ smart-contract/SmartMeter.sol
@@ -1,4 +1,35 @@
-contract SmartMeter is ChainlinkClient{
+// SPDX-License-Identifier: None
+pragma solidity 0.8.7;
+
+import '@chainlink/contracts/src/v0.8/ChainlinkClient.sol';
+
+interface cETH {
+    function mint() external payable;
+    function exchangeRateCurrent() external returns (uint256);
+    function supplyRatePerBlock() external returns (uint256);
+    function redeem(uint) external returns (uint);
+    function redeemUnderlying(uint) external returns (uint);
+    function exchangeRateStored() external view returns (uint); 
+    function balanceOf(address owner) external view returns (uint balance);
+}
+
+contract SmartElectricity is ChainlinkClient{
+    using Chainlink for Chainlink.Request;
+    event RequestUnits(bytes32 indexed requestId, uint256 units);
+    event MyLog(string, uint256);
+
+    bytes32 private jobId;
+    uint256 private fee;
+
+    address payable public contract_owner;
+    address payable government;
+    address current_user;
+
+    uint ContractBalanceWithoutCompound;
+    uint price_per_unit;
+
+    cETH cToken = cETH(0xd6801a1DfFCd0a410336Ef88DeF4320D6DF1883e); // Rinkeby
+
     struct User{
         uint wallet;
         uint incentives;
@@ -9,42 +40,79 @@ contract SmartMeter is ChainlinkClient{
         uint[] timestamps;
     }
 
+    mapping(address=>User) mapUser;
+    mapping(address=>address) mapUnitsUser;
+    
+    constructor(){
+        contract_owner = payable(msg.sender);
+        government = payable(0xbDdBd8eA4A33C3bcb7451c9a2e14DaE571cD25dA);
+        setChainlinkToken(0x01BE23585060835E02B77ef475b0Cc51aA1e0709);
+        setChainlinkOracle(0xf3FBB7f3391F62C8fe53f89B41dFC8159EE9653f);
+        jobId = 'ca98366cc7314957b8c012c72f05aeeb';
+        fee = (1 * LINK_DIVISIBILITY) / 10;
+    }
+
+    // Set Price per Unit
     function setPricePerUnit(uint _price_in_wei) public{
-        price = Set price of one unit
+        price_per_unit = _price_in_wei;
     }
 
-    function requestUnitsData(string memory _path) public{
-        units = request units consumed by user from API
+    // Chainlink
+    function requestUnitsData(string memory _path) public returns (bytes32 requestId) {
+        Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        req.add('get', 'https://db-rony.herokuapp.com/');
+        req.add('path', _path);
+        int256 timesAmount = 1;
+        req.addInt('times', timesAmount);
+        return (sendChainlinkRequest(req, fee));
     }
 
-    function payBill(string memory _user) public{
+    function fulfill(bytes32 _requestId, uint256 _units) public recordChainlinkFulfillment(_requestId) {
+        emit RequestUnits(_requestId, _units);
+        mapUser[current_user].units = _units;
+        mapUser[current_user].allUnits.push(_units);
+    }
+
+    function withdrawLink() public {
+        require(msg.sender == contract_owner);
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(link.transfer(msg.sender, link.balanceOf(address(this))), 'Unable to transfer');
+    }
+
+    // Compound
+    function payBill(string memory _user) public payable returns(bool){
         // Chainlink
         requestUnitsData(_user);
 
         // Set User Data
         current_user = msg.sender;
-        amount_to_pay = units*price_per_unit;
-        if(wallet>0){
-            amount_to_pay -= mapUser[msg.sender].wallet;
-            wallet = 0;
+        mapUser[msg.sender].amount_to_pay = mapUser[msg.sender].units*price_per_unit;
+        if(mapUser[msg.sender].wallet>0){
+            mapUser[msg.sender].amount_to_pay -= mapUser[msg.sender].wallet;
+            mapUser[msg.sender].wallet = 0;
         }
-        if(incentives>=500){
-            amount_to_pay -= incentives;
-            incentives = 0;
+        if(mapUser[msg.sender].incentives>=500){
+            mapUser[msg.sender].amount_to_pay -= mapUser[msg.sender].incentives;
+            mapUser[msg.sender].incentives = 0;
         }
-        if(amount_to_pay>=msg.value){
-            amount_to_pay = amount_to_pay-msg.value;
+        if(mapUser[msg.sender].amount_to_pay>=msg.value){
+            mapUser[msg.sender].amount_to_pay = mapUser[msg.sender].amount_to_pay-msg.value;
         }
-        if(amount_to_pay<msg.value){
-            wallet = msg.value-amount_to_pay;
-            amount_to_pay = 0;
+        if(mapUser[msg.sender].amount_to_pay<msg.value){
+            mapUser[msg.sender].wallet = msg.value-mapUser[msg.sender].amount_to_pay;
+            mapUser[msg.sender].amount_to_pay = 0;
         }
-        payments.push(msg.value);
-        timestamps.push(block.timestamp);
+        mapUser[msg.sender].payments.push(msg.value);
+        mapUser[msg.sender].timestamps.push(block.timestamp);
         ContractBalanceWithoutCompound += msg.value;
 
         // Compound
-        Send ETH to Compound using mint function
+        uint256 exchangeRateMantissa = cToken.exchangeRateCurrent();
+        emit MyLog("Exchange Rate (scaled up by 1e18): ", exchangeRateMantissa);
+        uint256 supplyRateMantissa = cToken.supplyRatePerBlock();
+        emit MyLog("Supply Rate: (scaled up by 1e18)", supplyRateMantissa);
+        cToken.mint{ value: msg.value, gas: 250000 }();
+        return true;
     }
 
     function redeemCETH() private returns (bool) {
@@ -59,7 +127,27 @@ contract SmartMeter is ChainlinkClient{
         contract_owner.transfer(address(this).balance);
     }
 
-    function getUserData(address _user) public{
-        return Struct User;
+    // Get User Data
+    function getUserData(address _user) public view returns(uint,uint,uint,uint,uint[] memory,uint[] memory,uint[] memory){
+        return (mapUser[_user].wallet,mapUser[_user].incentives,mapUser[_user].units,mapUser[_user].amount_to_pay,mapUser[_user].allUnits,mapUser[_user].payments,mapUser[_user].timestamps);
     }
+
+    // Get Balance
+    function getGovernmentBalance() public view returns(uint){
+        return ContractBalanceWithoutCompound;
+    }
+
+    function getContractBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function getUserCompoundBalance(address _userAddress) public view returns(uint) {
+        return cToken.balanceOf(_userAddress) * cToken.exchangeRateStored() / 1e18;
+    }
+
+    function getCompoundBalance() public view returns(uint) {
+        return cToken.balanceOf(address(this)) * cToken.exchangeRateStored() / 1e18;
+    }
+
+    receive() external payable {}
 }
